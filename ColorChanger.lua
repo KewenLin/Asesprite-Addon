e6392455be32200fcf706f8ea6119678c8caff88
Kewen Lin <92813375+4known@users.noreply.github.com>
2024-01-07 21:30:06 -0500

    Update ColorChanger.lua

--- ColorChanger.lua
+++ ColorChanger.lua
@@ -11,13 +11,13 @@ function changeToCanvasPosition()
 
     local image = cel.image
   
-    for y = 0, image.height - 1 do
-        for x = 0, image.width - 1 do
+    for x = 0, image.width - 1 do
+        for y = 0, image.height - 1 do
             local pixelValue = image:getPixel(x, y)
             local alpha = app.pixelColor.rgbaA(pixelValue)
             if alpha >= 1 then
                 local r = x
-                local g = y + 2
+                local g = y
                 local color = Color(r, g, 0, 255)
                 image:drawPixel(x, y, color)
             end
