052f2e155ea0c9170256e267a5c609e744831077
Kewen Lin <92813375+4known@users.noreply.github.com>
2024-01-07 22:48:13 -0500

    Update ColorChanger.lua

--- ColorChanger.lua
+++ ColorChanger.lua
@@ -10,19 +10,29 @@ function changeToCanvasPosition()
     end
 
     local image = cel.image
+    local rect = image.bounds
     local blueValue
-    for x = 0, image.width - 1 do
+    for x = 0, sprite.width - 1 do
         blueValue = x % 2 == 0 and 255 or 0
-        for y = 0, image.height - 1 do
-            local pixelValue = image:getPixel(x, y)
-            local alpha = app.pixelColor.rgbaA(pixelValue)
-            blueValue = blueValue == 0 and 255 or 0
-            if alpha >= 1 then
-                local color = Color(x, y, blueValue, 255)
-                image:drawPixel(x, y, color)
+        for y = 0, sprite.height - 1 do
+            if isInsideRectangle(x,y,rect.x,rect.y,rect.w,rect.h) then
+                local pixelValue = image:getPixel(x, y)
+                local alpha = app.pixelColor.rgbaA(pixelValue)
+                blueValue = blueValue == 0 and 255 or 0
+                if alpha >= 1 then
+                    local color = Color(x, y, blueValue, 255)
+                    image:drawPixel(x, y, color)
+                end
             end
         end
     end
+end
+
+function isInsideRectangle(x, y, rectX, rectY, rectWidth, rectHeight)
+    local rectRight = rectX + rectWidth
+    local rectBottom = rectY + rectHeight
+
+    return x >= rectX and x <= rectRight and y >= rectY and y <= rectBottom
 end
 
 function createDialogue()
