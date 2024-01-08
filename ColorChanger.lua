1da7600baaa2376564befe99183a9fe9822654fc
Kewen Lin <92813375+4known@users.noreply.github.com>
2024-01-07 21:04:56 -0500

    Update ColorChanger.lua

--- ColorChanger.lua
+++ ColorChanger.lua
@@ -1,13 +1,10 @@
-
 function changeToCanvasPosition()
     local sprite = app.activeSprite
-
     if sprite == nil then
         return
     end
 
     local cel = app.activeCel
-
     if cel == nil then
         return
     end
@@ -17,15 +14,31 @@ function changeToCanvasPosition()
     for y = 0, image.height - 1 do
         for x = 0, image.width - 1 do
             local pixel = image:getPixel(x, y)
-            if pixel.alpha == 255 then
-                local color = getColorByGridPosition(x, y)
+            if pixel.alpha >= 1 then
+                local r = x
+                local g = y
+                local color = Color(r, g, 0, 255)
                 image:drawPixel(x, y, color)
             end
         end
     end
-
 end
 
-do
-  changeToCanvasPosition()
+function createDialogue()
+    local dlg
+    dlg =
+    Dialog {
+    title = "Color As Position"
+    }
+    
+    dlg:
+    button {
+        id = "change",
+        text = "Change Color",
+        onclick = function()
+            changeToCanvasPosition()
+        end
+    }
 end
+
+createDialogue()
